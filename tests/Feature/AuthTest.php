ae6529c46ff29423a624ffb7219a9a71da5b337b
roginho <rogerbls51@gmail.com>
2021-10-23 14:13:07 -0300

    Testes login

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -8,11 +8,7 @@ use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+   
     public function test_login_true()
     {
         $user = User::first();
@@ -22,5 +18,28 @@ class AuthTest extends TestCase
         ];
         $response = $this->post('api/login', $credenciais);
         $response->assertStatus(200);
+    }
+
+    public function test_login_false(){
+
+        $user = User::first();
+        $credenciais = [
+            'email' =>$user->email,
+            'password' => 'teste123',
+        ];
+        $response = $this->post('api/login', $credenciais);
+        $response->assertStatus(401);
+    }
+
+    public function test_login_sem_dados(){
+
+        $user = User::first();
+        $credenciais = [];
+        $response = $this->post('api/login', $credenciais);
+        $response->assertStatus(422);
+        $dados = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('email',$dados);
+        $this->assertArrayHasKey('password',$dados);
+
     }
 }
